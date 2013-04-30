d04132468214973dbc589f2b8600c35de1dddf0c
Chris Mather <mather.chris@gmail.com>
2013-04-29 19:24:58 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to html5-file-api.";
+  var BinaryFileReader = {
+    read: function (file, callback) {
+      var reader = new FileReader;
+
+      var fileInfo = {
+        name: file.name,
+        type: file.type,
+        size: file.size,
+        file: null
+      };
+
+      // called when the file has been read into
+      // memory
+      reader.onload = function () {
+        // the result is assigned to the reader.result property and
+        // then we assign it to our fileInfo object and call our callback
+        // with the fileInfo passed as the second parameter
+        fileInfo.file = reader.result;
+        callback(null, fileInfo);
+      };
+
+      // called when there is an error reading
+      // the file into memory
+      reader.onerror = function () {
+        // if there's an error pass it as the first parameter to our
+        // callback
+        callback(reader.error);
+      };
+
+      // read the file into memory in a specified format like
+      // readAsText, reasAsBinaryString, readAsDataURL,
+      // readAsArrayBuffer
+      reader.readAsArrayBuffer(file);
+    }
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+  Template.fileUpload.events({
+    // Handle the form submit event
+    "submit form": function (e, tmpl) {
+      var fileInput = tmpl.find('input[type=file]');
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+      // grab a list of the files selected with the file chooser
+      // input
+      var fileList = fileInput.files;
+
+      // let's not actually submit the form
+      e.preventDefault();
+
+      for (var i = 0; i < fileList.length; i++) {
+
+        // for each file call the read method on our custom
+        // file reader object
+        BinaryFileReader.read(fileList[i], function (err, fileInfo) {
+          if (err)
+            console.log(err);
+          else
+            console.log(fileInfo);
+        });
+      }
+    }
   });
 }
